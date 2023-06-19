a124408b53e55e233cdd4670274569025537e2af
Mikhail <bloodsmiley130391@gmail.com>
2023-06-19 17:36:55 +0300

    add GHPage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+    },
+  ],
+  { basename: "/portfolio" }
+)
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
