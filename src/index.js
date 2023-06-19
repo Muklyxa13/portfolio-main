e7a6db98671e2ee661203216bbdc06cbed93f5ec
Mikhail <bloodsmiley130391@gmail.com>
2023-06-19 17:45:12 +0300

    change basename rout

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const router = createBrowserRouter(
       element: <App />,
     },
   ],
-  { basename: "/portfolio" }
+  { basename: "/portfolio-main" }
 )
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
